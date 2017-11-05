efce85b6dec7d9286ac72b14ec7194e32c047981
Rafael Biriba <biribarj@gmail.com>
2017-11-05 22:29:26 +0100

    Adding allowed_domain feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   has_many :projects, foreign_key: :owner_id
   has_and_belongs_to_many :subscriptions, class_name: "Project"
 
+  validate :allowed_domain
+
   validates_presence_of :name
   validates_uniqueness_of :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  private
+  def allowed_domain
+    return if Settings.allowed_domain.blank?
+    errors.add(:email, "domain not allowed") unless self.email.match("@#{Settings.allowed_domain}")
+  end
 end
