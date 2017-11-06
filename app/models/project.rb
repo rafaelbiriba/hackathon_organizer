775cc38d81cb38ce998b65e206943c5b8670f4ec
Rafael Biriba <biribarj@gmail.com>
2017-11-06 09:37:35 +0100

    Adding few more validations at Project form

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_and_belongs_to_many :subscribers, class_name: "User"
+
+  validates_presence_of  :title, :description
 end
