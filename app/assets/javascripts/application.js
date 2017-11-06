e9274993ae75736ecba18ee7cc00e6b098b4c1ae
Rafael Biriba <biribarj@gmail.com>
2017-11-06 11:23:15 +0100

    Few more projects layout adjusts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+
+$( document ).ready(function() {
+    $("[data-on-click]").on("click", function(e){
+      window.location.href = $(this).data("on-click");
+    })
+});
