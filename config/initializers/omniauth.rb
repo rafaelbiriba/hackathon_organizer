a0132ec39d06c0bb0f7c5deb4302927d4702802c
Rafael Biriba <biribarj@gmail.com>
2017-11-07 17:15:16 +0100

    Fixing bug on the projects show at subscribers section

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
   Settings.google_oauth_id,
-  Settings.google_oauth_secret, scope: 'email,profile', hd: Settings.allowed_domain
+  Settings.google_oauth_secret, scope: 'email,profile', hd: Settings.allowed_domain, image_aspect_ratio: "square"
 end
 
 # Disable raise values at development env (/auth/failure must handle the errors)
