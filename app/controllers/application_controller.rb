e6481d5c2786732cee784fc98f9801ee6611dbbf
Rafael Biriba <biribarj@gmail.com>
2017-11-07 23:48:05 +0100

    Remove develop line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    session[:current_user_id] = 1
     @current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
   end
 end
