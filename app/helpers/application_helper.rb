cc17361635cffc7fb31e2f4b13454c1b4052fbaa
Rafael Biriba <biribarj@gmail.com>
2017-11-07 19:17:40 +0100

    Improve admin power buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     @current_user
   end
 
+  def current_user_admin_power
+    current_user.is_admin && params[:admin] == "power"
+  end
+
   def human_readable_date(date)
     date.strftime("%m/%d/%Y %H:%M:%S")
   end
