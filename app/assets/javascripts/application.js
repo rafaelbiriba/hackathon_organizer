83a09b7dc76757936dda88e0ea9803fa9c960ba0
Rafael Biriba <biribarj@gmail.com>
2017-11-08 11:09:47 +0100

    Adding notification badge to the title of the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,11 +5,26 @@
 $( document ).ready(function() {
   $("[data-on-click]").on("click", function(e){
     window.location.href = $(this).data("on-click");
-  })
+  });
+
+  var page_title = document.title;
+
+  function update_title_with_notification_count(){
+    var notifications_count = $( ".navbar-nav .badge.notifications_count" ).text();
+    if(parseInt(notifications_count) > 0) {
+      document.title = '(' + notifications_count + ') ' + page_title;
+    } else {
+      document.title = page_title;
+    }
+  };
+
+  update_title_with_notification_count();
 
   setInterval(function(){
     $.get( "/notifications/counter.json", function( data ) {
-      $( ".navbar-nav .badge.notifications_count" ).html( data.notifications_count );
+      var notifications_count = data.notifications_count;
+      $( ".navbar-nav .badge.notifications_count" ).html(notifications_count);
+      update_title_with_notification_count();
     });
-  }, 180000); //3min
+  }, 120000); //2min
 });
