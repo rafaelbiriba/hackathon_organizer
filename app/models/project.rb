5d9f6ef8394d135fda26e37f5c5dad9bea0416b9
Rafael Biriba <biribarj@gmail.com>
2017-11-09 23:41:16 +0100

    Remove comments and notifications if a project is deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,20 @@ class Project < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_and_belongs_to_many :subscribers, class_name: "User"
   has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates_presence_of  :title, :description
+
+  before_destroy :destroy_notifications
 
   def all_involved_users(except_user: nil, include_commenters: false)
     users = [owner] + subscribers
     users = users + comments.collect(&:owner) if include_commenters
     users = users - [except_user]
     users.uniq
+  end
+
+  def destroy_notifications
+    Notification.where(project: self).destroy_all
   end
 end
