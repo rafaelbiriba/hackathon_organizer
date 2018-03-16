353c8033367dc47199b12b2ff3d6492289499228
Rafael Biriba <biribarj@gmail.com>
2018-03-15 21:05:44 -0300

    Disabling counter polling if not logged

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,15 @@ $( document ).ready(function() {
     }
   };
 
-  update_title_with_notification_count();
+  if($("p.avatar").length > 0) {
+    update_title_with_notification_count();
 
-  setInterval(function(){
-    $.get( "/notifications/counter.json", function( data ) {
-      var notifications_count = data.notifications_count;
-      $( ".navbar-nav .badge.notifications_count" ).html(notifications_count);
-      update_title_with_notification_count();
-    });
-  }, 120000); //2min
+    setInterval(function(){
+      $.get( "/notifications/counter.json", function( data ) {
+        var notifications_count = data.notifications_count;
+        $( ".navbar-nav .badge.notifications_count" ).html(notifications_count);
+        update_title_with_notification_count();
+      });
+    }, 12000); //2min
+  }
 });
