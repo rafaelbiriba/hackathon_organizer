c55458a08e6419fe263dca3af75b5c6c5c64036f
Rafael Biriba <biribarj@gmail.com>
2018-03-22 16:42:33 -0300

    Bumping version 0.1.0

---
+++ config/initializers/app_version.rb
@@ -0,0 +1,4 @@
+module HackathonOrganizer; VERSION = '0.1.0'; end
+
+# This file was created automatically by auto_tag_version gem
+# Documentation at https://github.com/rafaelbiriba/auto_tag_version
