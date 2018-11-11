84f55fee1840852f743c5696965d74fa6c515d52
Rafael Biriba <biribarj@gmail.com>
2018-11-11 02:01:02 +0100

    Change version to alpha

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,4 +1,1 @@
-module HackathonOrganizer; VERSION = '0.5.1'; end
-
-# This file was created automatically by auto_tag_version gem
-# Documentation at https://github.com/rafaelbiriba/auto_tag_version
+module HackathonOrganizer; VERSION = '0.6.0-alpha'; end
