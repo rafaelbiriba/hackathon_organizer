96e7e1f97a8184117b33936961493b97b8e86974
Rafael Biriba <biribarj@gmail.com>
2018-11-14 15:46:05 -0800

    Adding the migration for the first edition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180820065613) do
+ActiveRecord::Schema.define(version: 20181114234340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
