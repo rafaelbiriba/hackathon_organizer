534e36262d6ac8e833d7ccfc9bd24753a8750297
Rafael Biriba <biribarj@gmail.com>
2018-11-14 15:57:00 -0800

    Dummy commit

--- db/migrate/20181114234340_create_first_edition.rb
+++ db/migrate/20181114234340_create_first_edition.rb
@@ -1,6 +1,7 @@
 class CreateFirstEdition < ActiveRecord::Migration[5.1]
   def up
     # Populate the current application with the first edition
+    
     return if Edition.count > 0
     edition = Edition.create!(title: "Hackathon Migration", registration_start_date: Time.now, start_date: Time.now, end_date: Time.now)
     Project.all.each do |p|
