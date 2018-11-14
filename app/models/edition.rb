51a8759a45d3551879e919d553b8999c4d90c08c
Rafael Biriba <biribarj@gmail.com>
2018-11-13 23:37:59 -0800

    Adding some edition model validations

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -12,18 +12,25 @@
 class Edition < ApplicationRecord
   has_many :projects, dependent: :destroy
 
-  #validate :starts_at_ranges, :finishes_at_ranges
+  validates_presence_of :title, :registration_start_date, :start_date, :end_date
+  validate :dates_should_not_overlap
 
   def active?
-    Time.now > registration_start_date && Time.now <= end_date
+    Time.now.between?(registration_start_date, end_date)
+  end
+
+  def self.active_now
+    find_by_date(Time.now)
+  end
+
+  def self.find_by_date(date)
+    where(':date BETWEEN registration_start_date AND end_date', date: date).first
   end
 
   private
-
-  # def starts_at_ranges
-  #   errors.add(:starts_at, "there is ")
-  # end
-  #
-  # def finishes_at_ranges
-  # end
+  def dates_should_not_overlap
+    errors.add(:registration_start_date, "there is an overlap of dates") if Edition.find_by_date(registration_start_date)
+    errors.add(:start_date, "there is an overlap of dates") if Edition.find_by_date(start_date)
+    errors.add(:end_date, "there is an overlap of dates") if Edition.find_by_date(end_date)
+  end
 end
